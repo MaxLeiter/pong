9f1db06fc55d9b352c8068382c1807873907cd88
Drew DeVault <sir@cmpwn.com>
2015-04-22 20:06:41 -0600

    Implement basic opponent AI

--- src/update.c
+++ src/update.c
@@ -1,11 +1,12 @@
 #include <knightos/system.h>
+#include <knightos/display.h>
+#include <stdbool.h>
 #include "globals.h"
 #include "sprites.h"
 #include "update.h"
 #include "input.h"
 
-void update() {
-	do_input();
+void update_ball() {
 	ball.x += ball_motion.x;
 	ball.y += ball_motion.y;
 	if (ball.x >= 96 - ball_width) {
@@ -19,5 +20,35 @@ void update() {
 	if (ball.y >= 64 - ball_height || ball.y <= 0) {
 		ball_motion.y = -ball_motion.y;
 	}
+}
+
+bool ai_frame = false;
+
+void update_ai() {
+	ai_frame = !ai_frame;
+	if (ai_frame) {
+		/* Note: signed comparison not working? */
+		if ((unsigned char)ball_motion.x < 128) {
+			if (ball.y > right_paddle.y) {
+				right_paddle.y++;
+			} else if (ball.y < right_paddle.y) {
+				right_paddle.y--;
+			}
+		} else {
+			const int target = DISPLAY_HEIGHT / 2 - paddle_right_height / 2;
+			if (right_paddle.y < target) {
+				right_paddle.y++;
+			}
+			if (right_paddle.y > target) {
+				right_paddle.y--;
+			}
+		}
+	}
+}
+
+void update() {
+	do_input();
+	update_ball();
+	update_ai();
 	ksleep(10);
 }
