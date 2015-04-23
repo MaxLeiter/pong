faff02b3b6c3bf3834c90a2a64f8bfabdeda2126
Drew DeVault <sir@cmpwn.com>
2015-04-22 20:07:11 -0600

    Minor style improvement

--- src/update.c
+++ src/update.c
@@ -26,22 +26,23 @@ bool ai_frame = false;
 
 void update_ai() {
 	ai_frame = !ai_frame;
-	if (ai_frame) {
-		/* Note: signed comparison not working? */
-		if ((unsigned char)ball_motion.x < 128) {
-			if (ball.y > right_paddle.y) {
-				right_paddle.y++;
-			} else if (ball.y < right_paddle.y) {
-				right_paddle.y--;
-			}
-		} else {
-			const int target = DISPLAY_HEIGHT / 2 - paddle_right_height / 2;
-			if (right_paddle.y < target) {
-				right_paddle.y++;
-			}
-			if (right_paddle.y > target) {
-				right_paddle.y--;
-			}
+	if (!ai_frame) {
+		return;
+	}
+	/* Note: signed comparison not working? */
+	if ((unsigned char)ball_motion.x < 128) {
+		if (ball.y > right_paddle.y) {
+			right_paddle.y++;
+		} else if (ball.y < right_paddle.y) {
+			right_paddle.y--;
+		}
+	} else {
+		const int target = DISPLAY_HEIGHT / 2 - paddle_right_height / 2;
+		if (right_paddle.y < target) {
+			right_paddle.y++;
+		}
+		if (right_paddle.y > target) {
+			right_paddle.y--;
 		}
 	}
 }
